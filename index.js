04f74adf501fb9baa03ea534fa48d09bc73d6a4a
BII KIPKOECH COLLINS <ckipkoech17@gmail.com>
2022-06-19 09:47:07 +0300

    commit tree 3b8ad3ccb826fe2bb774e9ca39654ca6690c84e8

--- index.js
+++ index.js
@@ -1,20 +1,46 @@
+
+
 function isPalindrome(word) {
-  // Write your algorithm here
+ 
+  for (let startIndex = 0; startIndex < word.length / 2; startIndex++) {
+   
+    const endIndex = word.length - 1 - startIndex;
+    if (word[startIndex] !== word[endIndex]) {
+      // if the letters don't match, return false
+      return false;
+    }
+  }
+
+  // if we reach the middle, and all the letters match, return true
+  return true;
 }
 
-/* 
-  Add your pseudocode here
-*/
 
-/*
-  Add written explanation of your solution here
-*/
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
-  // add your own custom tests in here
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("mom"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("hi"));
 
   console.log("");
 
